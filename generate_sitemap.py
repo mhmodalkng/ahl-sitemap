05171a062ddfa8fe6c52ed63b2f6b77650815b38
mhmodalkng <mahmoodboos123@gmail.com>
2025-05-21 20:32:38 +0300

    Update generate_sitemap.py

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -2,39 +2,37 @@ import requests
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
-BLOG_URL = "https://vlogars.blogspot.com"
+BLOG_ID = 'vlogars.blogspot.com'
 MAX_RESULTS = 150
-START_INDEX = 1
-XML_FILE = "sitemap.xml"
+SITEMAP_FILE = 'sitemap.xml'
 
-def fetch_posts(start_index):
-    url = f"{BLOG_URL}/feeds/posts/default?alt=json&start-index={start_index}&max-results={MAX_RESULTS}"
+def fetch_posts(start_index=1):
+    url = f'https://{BLOG_ID}/feeds/posts/default?alt=json&start-index={start_index}&max-results={MAX_RESULTS}'
     response = requests.get(url)
-    return response.json()
+    if response.status_code != 200:
+        return []
+    data = response.json()
+    return data.get('feed', {}).get('entry', [])
+
+def extract_url_and_date(entry):
+    link = next((l['href'] for l in entry['link'] if l['rel'] == 'alternate'), None)
+    updated = entry['updated']['$t'].split('T')[0]
+    return link, updated
 
 def generate_sitemap():
-    ns = {'ns': 'http://www.w3.org/2005/Atom'}
-    urlset = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
-
-    index = START_INDEX
+    urlset = ET.Element('urlset', xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+    start_index = 1
     while True:
-        data = fetch_posts(index)
-        entries = data.get("feed", {}).get("entry", [])
+        entries = fetch_posts(start_index)
         if not entries:
             break
-
         for entry in entries:
-            loc = next(link['href'] for link in entry['link'] if link['rel'] == 'alternate')
-            lastmod = entry['updated']['$t'].split('T')[0]
-
-            url_el = ET.SubElement(urlset, "url")
-            ET.SubElement(url_el, "loc").text = loc
-            ET.SubElement(url_el, "lastmod").text = lastmod
-
-        index += MAX_RESULTS
-
+            loc, lastmod = extract_url_and_date(entry)
+            url_elem = ET.SubElement(urlset, 'url')
+            ET.SubElement(url_elem, 'loc').text = loc
+            ET.SubElement(url_elem, 'lastmod').text = lastmod
+        start_index += MAX_RESULTS
     tree = ET.ElementTree(urlset)
-    tree.write(XML_FILE, encoding="utf-8", xml_declaration=True)
+    tree.write(SITEMAP_FILE, encoding="utf-8", xml_declaration=True)
 
-if __name__ == "__main__":
-    generate_sitemap()
+generate_sitemap()
